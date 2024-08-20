bc656eb51e351ce16017c45d576825360ba001bf
Azharawan <azhar34658@gmail.com>
2024-08-20 13:06:50 +0500

    About pakistan(tailwindcss)

---
+++ About pakistan/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"], // This includes all HTML files in the src folder and its subfolders
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
